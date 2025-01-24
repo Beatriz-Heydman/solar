3e8f76685ae44ca86ab3586320cb134afa0ba7b4
Beatriz Heydman <biaheydman@outlook.com>
2025-01-23 21:47:50 -0300

    feat: add theme olors and font family

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"], // add your preferred file extensions here
+  theme: {
+    colors: {
+      white: "#ffffff",
+      blue: "#BAA333",
+      black: "#000000",
+      yellow: "#F2CC37",
+      light_gray: "#9AA4AC",
+      medium_gray: "#4C5861",
+      dark_gray: "#051A2E",
+      gray_background: "#F1F3F9",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+        "plus-jakarta-sans": ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
